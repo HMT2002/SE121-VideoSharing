58ab311e7c96d33e8a16c2b303ad13d0dc915cc5
phatlam1811 <phatlam1811@gmail.com>
2023-04-21 15:20:27 +0700

    re-factor frontend & add LoginPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,11 @@
-import './App.css';
-import React, { useState, useEffect, useCallback, useContext } from 'react';
+import React from "react";
 
-import NewThread from './pages/NewThread/NewThread';
-import Welcome from './pages/Welcome/Welcome';
-import TestPage from './pages/TestPage/TestPage';
-import SignIn from './pages/SignIn/SignIn';
-import SignUp from './pages/SignUp/SignUp';
-import Thread from './pages/Thread/Thread';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import ProtectedRoute from './components/Route/ProtectRoute';
-import AuthContext from './store/auth-context';
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  const authCtx = useContext(AuthContext);
-  console.log(authCtx.isLoggedIn);
-  console.log(authCtx.role);
-
   return (
     <React.Fragment>
-      <Routes>
-        <Route path="/" exact element={<Welcome />}></Route>
-        <Route path="*" element={<Navigate to="/" replace />} />
-        {/* <Route path="/threads" exact element={<Welcome />}></Route> */}
-        <Route path="/user-info" exact element={!authCtx.isLoggedIn && <Welcome />}></Route>
-        <Route
-          path="/create-thread"
-          exact
-          element={
-            authCtx.isLoggedIn && (authCtx.role === 'content-creator' || authCtx.role === 'admin') ? (
-              <NewThread />
-            ) : (
-              <Navigate to="/" />
-            )
-          }
-        ></Route>
-        <Route path="/test" exact element={<TestPage />}></Route>
-        <Route path="/sign/in" exact element={authCtx.role === 'guest' ? <SignIn /> : <Navigate to="/" />}></Route>
-        <Route path="/sign/up" exact element={authCtx.role === 'guest' ? <SignUp /> : <Navigate to="/" />}></Route>
-        <Route path="/threads/:slug" exact element={<Thread />}></Route>
-      </Routes>
+      <LoginPage />
     </React.Fragment>
   );
 }
---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Card from "./Card";
+import TextField from "@mui/material/TextField";
+
+import "../styles/LoginForm.css";
+
+const LoginForm = () => {
+    return (
+        <Card className="login-form">
+            <form>
+                <div className="login-form__title">Account Login</div>
+                <div style={{ height: "30px" }} />
+                <div className="login-form__input">
+                    <TextField className="login-form__input-textfield" label="Username" variant="standard" />
+                </div>
+                <div className="login-form__input">
+                    <TextField className="login-form__input-textfield" label="Password" variant="standard" type="password" />
+                </div>
+                <div className="login-form__forget-password-redirect">Forgot password?</div>
+                <button className="login-form__login-btn" type="submit" >LOGIN</button>
+                <button className="login-form__register-btn">CREATE NEW ACCOUNT</button>
+            </form>
+        </Card>
+    );
+}
+
+export default LoginForm;

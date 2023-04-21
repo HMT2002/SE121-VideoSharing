fcca2644c18725e7913e958d8f59b51514c344ab
phatlam1811 <phatlam1811@gmail.com>
2023-04-22 00:37:02 +0700

    add auth-apis.js (auth actions) but failed to work

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import Card from "./Card";
@@ -6,16 +6,45 @@ import TextField from "@mui/material/TextField";
 
 import "../styles/LoginForm.css";
 
-const LoginForm = () => {
+const LoginForm = (props) => {
+    const [enteredEmail, setEnteredEmail] = useState("");
+    const [enteredPassword, setEnteredPassword] = useState("");
+
+    const EmailInputChangeHandler = (event) => {
+        setEnteredEmail(event.target.value);
+    }
+
+    const PasswordInputChangeHandler = (event) => {
+        setEnteredPassword(event.target.value);
+    }
+
+    const LoginSubmitHandler = (event) => {
+        event.preventDefault();
+        props.onUserLogin(enteredEmail, enteredPassword);
+    }
+
     return (
         <Card className="login-form">
-            <form>
+            <form onSubmit={LoginSubmitHandler}>
                 <h1 className="login-form__title">Account Login</h1>
                 <div className="login-form__input">
-                    <TextField className="login-form__input-textfield" label="Username" variant="standard" />
+                    <TextField
+                        className="login-form__input-textfield"
+                        label="Username"
+                        variant="standard"
+                        value={enteredEmail}
+                        onChange={EmailInputChangeHandler}
+                    />
                 </div>
                 <div className="login-form__input">
-                    <TextField className="login-form__input-textfield" label="Password" variant="standard" type="password" />
+                    <TextField
+                        className="login-form__input-textfield"
+                        label="Password"
+                        variant="standard"
+                        type="password"
+                        value={enteredPassword}
+                        onChange={PasswordInputChangeHandler}
+                    />
                 </div>
                 <Link className="login-form__forget-password-redirect" to="/">Forgot password?</Link>
                 <button className="login-form__login-btn" type="submit" >LOGIN</button>
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+// import { LoginAction } from "../APIs/auth-apis";
+
 import LoginForm from "../components/LoginForm";
 
 const LoginPage = () => {
+    const UserLoginHandler = async (username, password) => {
+        // const response = await LoginAction({ username, password }); -- this currently fail
+        console.log("username : ", username);
+        console.log("password : ", password);
+    }
+
     return (
-        <LoginForm />
+        <LoginForm onUserLogin={UserLoginHandler} />
     );
 }
 

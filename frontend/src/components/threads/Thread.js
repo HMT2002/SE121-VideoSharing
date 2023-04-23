3cd0ca49f1dc516bb6388938b5dbaa425c49f66e
phatlam1811 <phatlam1811@gmail.com>
2023-04-24 05:37:10 +0700

    homepage basic frontend done

--- frontend/src/components/threads/Thread.js
+++ frontend/src/components/threads/Thread.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
 import { format, getDate } from "date-fns";
 
 import Card from "../../UI elements/Card";
@@ -23,26 +24,28 @@ const Thread = (props) => {
 
     return (
         <Card className="thread">
-            <div className="thread-creator">
+            <Link className="thread" to="/">
+                <div className="thread-creator">
+                    <img
+                        className="thread-creator avatar"
+                        src={threadCreator.photo.link}
+                        alt="Creator's avatar"
+                    />
+                    <h1 className="thread-creator display-name">
+                        {threadCreator.username}
+                    </h1>
+                </div>
                 <img
-                    className="thread-creator avatar"
-                    src={threadCreator.photo.link}
-                    alt="Creator's avatar"
+                    className="thread-video-thumbnail"
+                    src={threadVideo.thumbLink}
+                    alt="Thread video thumbnail"
                 />
-                <h1 className="thread-creator display-name">
-                    {threadCreator.username}
-                </h1>
-            </div>
-            <img
-                className="thread-video-thumbnail"
-                src={threadVideo.thumbLink}
-                alt="Thread video thumbnail"
-            />
-            <div className="thread-summary">
-                <h1 className="thread-summary title">{thread.title}</h1>
-                <p className="thread-summary content">{thread.content}</p>
-            </div>
-            <h1 className="thread-create-date">{threadCreatedDate}</h1>
+                <div className="thread-summary">
+                    <h1 className="thread-summary title">{thread.title}</h1>
+                    <p className="thread-summary content">{thread.content}</p>
+                </div>
+                <h1 className="thread-create-date">{threadCreatedDate}</h1>
+            </Link>
         </Card>
     );
 };
--- frontend/src/components/threads/ThreadList.js
+++ frontend/src/components/threads/ThreadList.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import Thread from "./Thread";
 
+import "../../styles/ThreadList.css";
+
 const ThreadList = (props) => {
     const threads = props.threads.map((thread) => (
-        <Thread
-            key={props.threads.indexOf(thread)}
-            thread={thread}
-        />
+        <li key={props.threads.indexOf(thread)}>
+            <Thread thread={thread} />
+        </li>
     ));
     return (
         <ul className="thread-list">

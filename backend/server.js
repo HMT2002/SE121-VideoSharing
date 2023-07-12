2605b54e42ee9637b928489bcbcb77a0bdf24044
DesDesDesHMT <82308907+HMT2002@users.noreply.github.com>
2023-07-12 23:35:41 +0700

    t mệt rồi

--- backend/server.js
+++ backend/server.js
@@ -22,16 +22,16 @@ const server= app.listen(port, () => {
 new hls(server,{
   provider:{
     exists:(req,cb)=>{
-      console.log('server js '+ req.url)
-      const ext=req.url.split('.').pop();
-      console.log(req.url);
+      console.log('server js exists'+ req.url)
+      const ext=req.url.split('.')[1];
+      const filename=req.url.split('.')[0];
       if(ext!=='m3u8'&&ext!=='ts'){
         return cb(null,true);
       }
 
-      fs.access(__dirname+'/videos/convert/7.m3u8',fs.constants.F_OK,function(err){
+      fs.access(__dirname+'/videos/convert/'+filename+'.m3u8',fs.constants.F_OK,function(err){
         if(err){
-          console.log(__dirname+'/videos/convert/7.m3u8');
+          console.log(__dirname+'/videos/convert/'+filename+'.m3u8');
           console.log( 'File not exist');
           return cb(null,false);
         }
@@ -41,13 +41,13 @@ new hls(server,{
       })
     },
     getManifestStream:(req,cb)=>{
-      console.log('server js '+ req.url)
+      console.log('server js getManifestStream '+ req.url)
 
       const stream=fs.createReadStream(__dirname+req.url);
       cb(null,stream);
     },
     getSegmentStream:(req,cb)=>{
-      console.log('server js '+ req.url)
+      console.log('server js getSegmentStream '+ req.url)
       const stream=fs.createReadStream(__dirname+req.url);
       cb(null,stream);
     },

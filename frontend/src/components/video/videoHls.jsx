e3493d830de7a1a3706ec5576b1e1905b1c5a0c3
DesDesDesHMT <82308907+HMT2002@users.noreply.github.com>
2023-07-26 22:19:20 +0700

    add event status

--- frontend/src/components/video/videoHls.jsx
+++ frontend/src/components/video/videoHls.jsx
@@ -22,9 +22,25 @@
 import './videoHls.css'
 import React, { useContext, useEffect, useState, useRef, Component } from 'react';
 import Hls from 'hls.js';
+function customLogger(logContent){
+      const color = "color: white; background-color: black;" 
+
+        console.log(`%c${logContent}`, `${color} font-size: 13px; text-align: center; padding: 12px 100px; display:block`);
+}
+const customeBoxMessage=(prevState,className,content)=>{
+  return (<React.Fragment>{prevState}<br/>
+  <div className={className}>{content}</div>
+  <br/></React.Fragment>)
+}
+
+const decodeString=(encodeURI)=>{
+  return decodeURIComponent(encodeURI);
+}
 const VideoHls = (props) => {
   const player = useRef();
   const [logger,setLogger]=useState('');
+  const [eventLogger,setEventLogger]=useState('');
+
 
   useEffect(() => {
     const loadVideo = async () => {
@@ -48,8 +64,8 @@ const VideoHls = (props) => {
       hls.loadSource(url);
       hls.attachMedia(video);
       hls.on(Hls.Events.MANIFEST_PARSED, function () {
-        console.log(player);
-        setLogger((prevState)=>{return (<React.Fragment>{prevState}<br/>{player.current.baseURI}<br/>{player.current.currentSrc}</React.Fragment>)});
+        console.log(player)
+        setLogger((prevState)=>{return customeBoxMessage(prevState,'logger-message-MANIFEST_PARSE',(<>{decodeString(player.current.baseURI) }<br/>{decodeString( player.current.currentSrc)}</>))});
         try {
           video.play();
         } catch (error) {
@@ -59,65 +75,64 @@ const VideoHls = (props) => {
 
       hls.on(Hls.Events.MANIFEST_LOADED, function () {
         try {
-          setLogger((prevState)=>{return (<React.Fragment>{prevState}<br/><div className='logger-message-MANIFEST_LOADED'>Events.MANIFEST_LOADED</div><br/></React.Fragment>)});
+          setLogger((prevState)=>{return customeBoxMessage (prevState,'logger-message-MANIFEST_LOADED',(<>Events.MANIFEST_LOADED</>))});
         } catch (error)
         {
-          setLogger((prevState)=>{return (<React.Fragment><div className='logger-message-MANIFEST_LOADED'>{prevState}<br/>{error}</div><br/></React.Fragment>)})
+          setLogger((prevState)=>{return customeBoxMessage (prevState,'logger-message-MANIFEST_LOADED',(<>Events.MANIFEST_LOADED<br/>Error:{error}</>))});
         }
       });    
 
       hls.on(Hls.Events.FRAG_LOADED, function () {
         try {
-          setLogger((prevState)=>{return (<React.Fragment>{prevState}<br/><div className='logger-message-FRAG_LOADED'>Events.FRAG_LOADED</div><br/></React.Fragment>)});
+          setLogger((prevState)=>{return customeBoxMessage(prevState,'logger-message-FRAG_LOADED',(<>Events.FRAG_LOADED</>))});
         } catch (error)
         {
-          setLogger((prevState)=>{return (<React.Fragment><div className='logger-message-FRAG_LOADED'>{prevState}<br/>{error}</div><br/></React.Fragment>)})
+          setLogger((prevState)=>{return customeBoxMessage (prevState,'logger-message-FRAG_LOADED',(<>Events.FRAG_LOADED<br/>Error:{error}</>))});
         }
       });    
 
       hls.on(Hls.Events.FRAG_BUFFERED, function () {
         try {
-          setLogger((prevState)=>{return (<React.Fragment>{prevState}<br/><div className='logger-message-FRAG_BUFFERED'>Events.FRAG_BUFFERED</div><br/></React.Fragment>)});
+          setLogger((prevState)=>{return customeBoxMessage(prevState,'logger-message-FRAG_BUFFERED',(<>Events.FRAG_BUFFERED</>))});
         } catch (error)
         {
-          setLogger((prevState)=>{return (<React.Fragment><div className='logger-message-FRAG_BUFFERED'>{prevState}<br/>{error}</div><br/></React.Fragment>)})
+          setLogger((prevState)=>{return customeBoxMessage (prevState,'logger-message-FRAG_BUFFERED',(<>Events.FRAG_BUFFERED<br/>Error:{error}</>))});
         }
       });    
 
       hls.on(Hls.Events.FRAG_CHANGED, function () {
         try {
-          setLogger((prevState)=>{return (<React.Fragment>{prevState}<br/><div className='logger-message-FRAG_CHANGED'>Events.FRAG_CHANGED</div><br/></React.Fragment>)});
+          setLogger((prevState)=>{return customeBoxMessage(prevState,'logger-message-FRAG_CHANGED',(<>Events.FRAG_CHANGED</>))});
         } catch (error)
         {
-          setLogger((prevState)=>{return (<React.Fragment><div  className='logger-message-FRAG_CHANGED'>{prevState}<br/>{error}</div><br/></React.Fragment>)})
+          setLogger((prevState)=>{return customeBoxMessage (prevState,'logger-message-FRAG_CHANGED',(<>Events.FRAG_CHANGED<br/>Error:{error}</>))});
         }
       });    
 
       hls.on(Hls.Events.FRAG_DECRYPTED, function () {
         try {
-          setLogger((prevState)=>{return (<React.Fragment>{prevState}<br/><div className='logger-message-FRAG_DECRYPTED'>Events.FRAG_DECRYPTED</div><br/></React.Fragment>)});
+          setLogger((prevState)=>{return customeBoxMessage(prevState,'logger-message-FRAG_DECRYPTED',(<>Events.FRAG_DECRYPTED</>))});
         } catch (error)
         {
-          setLogger((prevState)=>{return (<React.Fragment><div  className='logger-message-FRAG_DECRYPTED'>{prevState}<br/>{error}</div><br/></React.Fragment>)})
+          setLogger((prevState)=>{return customeBoxMessage (prevState,'logger-message-FRAG_DECRYPTED',(<>Events.FRAG_DECRYPTED<br/>Error:{error}</>))});
         }
       });    
 
       hls.on(Hls.Events.FRAG_LOADING, function () {
         try {
-          setLogger((prevState)=>{return (<React.Fragment>{prevState}<br/><div className='logger-message-FRAG_LOADING'>Events.FRAG_LOADING</div><br/></React.Fragment>)});
+          setLogger((prevState)=>{return customeBoxMessage(prevState,'logger-message-FRAG_LOADING',(<>Events.FRAG_LOADING</>))});
         } catch (error)
         {
-          setLogger((prevState)=>{return (<React.Fragment><div  className='logger-message-FRAG_LOADING'>{prevState}<br/>{error}</div><br/></React.Fragment>)})
+          setLogger((prevState)=>{return customeBoxMessage (prevState,'logger-message-FRAG_LOADING',(<>Events.FRAG_LOADING<br/>Error:{error}</>))});
         }
       });   
 
       hls.on(Hls.Events.FRAG_PARSED, function () {
         try {
-
-          setLogger((prevState)=>{return (<React.Fragment>{prevState}<br/><div className='logger-message-FRAG_PARSED'>Events.FRAG_PARSED</div><br/></React.Fragment>)});
+          setLogger((prevState)=>{return customeBoxMessage(prevState,'logger-message-FRAG_PARSED',(<>Events.FRAG_PARSED</>))});
         } catch (error)
         {
-        setLogger((prevState)=>{return (<React.Fragment><div className='logger-message-FRAG_PARSED'>{prevState}<br/>{error}</div><br/></React.Fragment>)})
+          setLogger((prevState)=>{return customeBoxMessage (prevState,'logger-message-FRAG_PARSED',(<>Events.FRAG_PARSED<br/>Error:{error}</>))});
         }
       });  
     };
@@ -128,11 +143,20 @@ const VideoHls = (props) => {
   return (
     <React.Fragment>
       <video ref={player} controls loop autoPlay={true} />
+      <div className='event-status'>
+        
+      <pre >0.039 | Loading https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8
+0.061 | Loading manifest and attaching video element...
+0.768 | 5 quality levels found
+0.769 | Manifest successfully loaded
+2.072 | Media element attached</pre>
+      </div>
       <div className='logger-box'>
         <div>Message, Event</div>
         <br />
         <div>{logger}</div>
       </div>
+
     </React.Fragment>
   );
 };

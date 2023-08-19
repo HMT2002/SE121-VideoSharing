f99afa50361c45a0d618b1d87a8df271c99deb05
DesDesDesHMT <82308907+HMT2002@users.noreply.github.com>
2023-08-19 21:45:10 +0700

    delete assjs

--- frontend/src/pages/VideoPage.jsx
+++ frontend/src/pages/VideoPage.jsx
@@ -1,57 +1,60 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from 'react';
 
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from 'react-router-dom';
 
-import Utils from "../Utils";
-import AuthContext from "../contexts/auth-context";
-import Card from "../components/UI elements/Card";
+import Utils from '../Utils';
+import AuthContext from '../contexts/auth-context';
+import Card from '../components/UI elements/Card';
 
-import "../styles/ThreadPage.css";
+import '../styles/ThreadPage.css';
 
 const VideoPage = () => {
+  const params = useParams();
+  const [source, setSource] = useState('/videos/convert/' + params.videoname + '.m3u8');
 
-    const params = useParams();
-    const [source,setSource]=useState('/videos/convert/'+params.videoname+'.m3u8');
+  useEffect(() => {
+    const LoadVideo = async () => {
+      try {
+        //   const response = await fetch('/api/test/video-convert/test-phase-convert-video/'+params.videoname, {
+        //     method: 'GET'
+        // });
+        // const data = await response.json();
+        // if(data.status){
+        //   if(data.status==='found and converted'){
+        //   }
+        //   else{
+        //   }
+        // }
+      } catch (error) {
+        console.log(error);
+      }
+      if (params.videoname === 'test-file-upload-formdata') {
+        // có khả năng nhận về file sub định dạng vtt vì bên server nginx có hỗ trợ host file toàn tập, node thì không thấy.
+        // url = 'http://192.168.140.104/tmp/convert/哀の隙間 - feat.初音ミク.m3u8';
+        const url = 'http://192.168.140.104/tmp/prep/fileformdata';
+        setSource((prevState) => url);
+        console.log(url);
+      } else {
+        setSource((prevState) => '/videos/convert/' + params.videoname + '.m3u8');
+      }
+    };
+    LoadVideo();
+  }, []);
 
-    // useEffect(() => {
-    //     const LoadVideo = async () => {
-    //         try {
-    //           const response = await fetch('/api/test/video-convert/test-phase-convert-video/'+params.videoname, {
-    //             method: 'GET'
-    //         });
-    //         const data = await response.json();
-    //         if(data.status){
-    //           if(data.status==='found and converted'){
-
-    //           }
-    //           else{
-                
-    //           }
-    //         }
-    //     }
-    //     catch(error){
-    //         console.log(error)
-    //     }
-    // }
-    //       LoadVideo();
-    //       setSource(prevState=>'/videos/convert/'+params.videoname+'.m3u8')
-    //     }, [params.videoname]);
-
-    return (
-        <React.Fragment>
-            <Card className="thread-page__thread">
-                <video
-                    className="video-js thread-page__thread-video"
-                    controls
-                    autoPlay={true}
-                    src={source}
-                    loop={true}
-                    type= "application/x-mpegURL"
-                     />
-
-            </Card>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Card className="thread-page__thread">
+        <video
+          className="video-js thread-page__thread-video"
+          controls
+          autoPlay={true}
+          src={source}
+          loop={true}
+          type="application/x-mpegURL"
+        />
+      </Card>
+    </React.Fragment>
+  );
 };
 
 export default VideoPage;

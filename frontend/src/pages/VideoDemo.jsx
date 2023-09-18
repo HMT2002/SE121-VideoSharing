9c86e28f29c3c646e0aa4995c6f35c09f95388d5
DesDesDesHMT <82308907+HMT2002@users.noreply.github.com>
2023-09-18 18:03:27 +0700

    stop dashjs logging debug

--- frontend/src/pages/VideoDemo.jsx
+++ frontend/src/pages/VideoDemo.jsx
@@ -17,13 +17,22 @@ import Button from '../components/UI elements/Button';
 
 import Utils from '../Utils';
 
+import dashjs from 'dashjs';
+import ControlBar from '../components/dashControlBar/ControlBar';
+import '../components/dashControlBar/controlbar.css';
+import '../components/dashControlBar/icomoon.ttf';
 import '../styles/ThreadPage.css';
 
 const VideoDemo = () => {
   const params = useParams();
   const [source, setSource] = useState('/videos/MY Heart Rate.mp4');
   const videoNormal = useRef();
-  const videoStreamingRTMPLinux = useRef();
+
+  const videoDashLinux = useRef();
+  const videoDashWindow = useRef(null);
+  const playerDashWindow = useRef(null);
+
+
   const videoHLS = useRef();
   const videoStreamingRTMPRed5 = useRef();
   const videoHLSRed5 = useRef();
@@ -32,30 +41,63 @@ const VideoDemo = () => {
     const LoadVideo = async () => {
       setSource((prevState) => '/videos/MY Heart Rate.mp4');
       try {
-        var obj_play_HLS = {
-          fill: true,
-          fluid: true,
-          autoplay: true,
-          controls: true,
-          preload: 'auto',
-          loop: true,
-          sources: [
-            {
-              src: '/videos/convert/MY Heart Rate.m3u8',
-              type: 'application/x-mpegURL',
-            },
-          ],
-        };
-        const _playerHLS = videojs(videoHLS.current, obj_play_HLS, function onPlayerReady() {
-          videojs.log('Your player is ready!');
-          const defaultVolume = 0.4;
-          this.volume(defaultVolume);
-          this.on('ended', function () {
-            videojs.log('Awww...over so soon?!');
-          });
-        });
+        // var obj_play_HLS = {
+        //   fill: true,
+        //   fluid: true,
+        //   autoplay: true,
+        //   controls: true,
+        //   preload: 'auto',
+        //   loop: true,
+        //   sources: [
+        //     {
+        //       src: '/videos/convert/MY Heart Rate.m3u8',
+        //       type: 'application/x-mpegURL',
+        //     },
+        //   ],
+        // };
+        // const _playerHLS = videojs(videoHLS.current, obj_play_HLS, function onPlayerReady() {
+        //   videojs.log('Your player is ready!');
+        //   const defaultVolume = 0.4;
+        //   this.volume(defaultVolume);
+        //   this.on('ended', function () {
+        //     videojs.log('Awww...over so soon?!');
+        //   });
+        // });
+
+        // const videoDashWindowCurrent=videoDashWindow.current;
+        // var urlDash = 'http://localhost/tmp_dash/videomusic1080/index.mpd';
+        // var playerDashWindow = dashjs.MediaPlayer().create();
+        // playerDashWindow.initialize(videoDashWindowCurrent, urlDash, true);
+        // playerDashWindow.current.attachView(videoDashWindowCurrent);
+        // console.log(playerDashWindow)
+
+
+        if (videoDashWindow.current) {
+          const video = videoDashWindow.current;
+          var urlDash = 'http://localhost/tmp_dash/videomusic1080/index.mpd';
+        playerDashWindow.current = dashjs.MediaPlayer().create();
+    
+        playerDashWindow.current.initialize(video, urlDash, true);
+        playerDashWindow.current.attachView(video);
+
+        console.log(playerDashWindow.current)
+
+        if(playerDashWindow.current){
+                  playerDashWindow.current.updateSettings({ 'debug': { 'logLevel': dashjs.Debug.LOG_LEVEL_NONE }});
+                  console.log(video)
+
+        }
+          const controlbar = new ControlBar(playerDashWindow.current);
+          //Player is instance of Dash.js MediaPlayer;
+          controlbar.initialize();
+        }
+
       } catch (error) {
         console.log(error);
+        if (playerDashWindow.current) {
+          playerDashWindow.current.destroy();
+          playerDashWindow.current = null;
+        }
       }
     };
 
@@ -73,6 +115,72 @@ const VideoDemo = () => {
           className="video-js thread-page__thread-video"
           src="http://localhost:5080/oflaDemo/ハルジオン.mp4"
         ></video>
+        <video ref={videoDashLinux} className="video-js"></video>
+        <div className="dash-video-player ">
+      <div className="videoContainer" id="videoContainer">
+        <video ref={videoDashWindow} ></video>
+        <div id="videoController" className="video-controller unselectable">
+          <div id="playPauseBtn" className="btn-play-pause" title="Play/Pause">
+            <span id="iconPlayPause" className="icon-play"></span>
+          </div>
+          <span id="videoTime" className="time-display">
+            00:00:00
+          </span>
+          <div
+            id="fullscreenBtn"
+            className="btn-fullscreen control-icon-layout"
+            title="Fullscreen"
+          >
+            <span className="icon-fullscreen-enter"></span>
+          </div>
+          <div
+            id="bitrateListBtn"
+            className="control-icon-layout"
+            title="Bitrate List"
+          >
+            <span className="icon-bitrate"></span>
+          </div>
+          <input
+            type="range"
+            id="volumebar"
+            className="volumebar"
+            min="0"
+            max="1"
+            step=".01"
+          />
+          <div id="muteBtn" className="btn-mute control-icon-layout" title="Mute">
+            <span id="iconMute" className="icon-mute-off"></span>
+          </div>
+          <div
+            id="trackSwitchBtn"
+            className="control-icon-layout"
+            title="A/V Tracks"
+          >
+            <span className="icon-tracks"></span>
+          </div>
+          <div
+            id="captionBtn"
+            className="btn-caption control-icon-layout"
+            title="Closed Caption"
+          >
+            <span className="icon-caption"></span>
+          </div>
+          <span id="videoDuration" className="duration-display">
+            00:00:00
+          </span>
+          <div className="seekContainer">
+            <div id="seekbar" className="seekbar seekbar-complete">
+              <div id="seekbar-buffer" className="seekbar seekbar-buffer"></div>
+              <div id="seekbar-play" className="seekbar seekbar-play"></div>
+            </div>
+          </div>
+          <div id="thumbnail-container" className="thumbnail-container">
+            <div id="thumbnail-elem" className="thumbnail-elem"></div>
+            <div id="thumbnail-time-label" className="thumbnail-time-label"></div>
+          </div>
+        </div>
+        </div>
+        </div>
       </Card>
     </React.Fragment>
   );
